6ad0dd3b994e846662536cc4b8e1f7f9d9548034
ElenderMytite <ElenderMytite>
2025-05-10 17:31:50 +0200

    square notation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+static SIZE: usize = 8;
+
 fn main() {
-    println!("Hello, world!");
+    let mut index;
+    for i in 0..SIZE{
+        for j in 0..SIZE {
+            index = i * SIZE + j;
+            println!("{}", index_to_position(index));
+        }
+    }  
+}
+fn index_to_position(index: usize) -> String {
+    let column: usize = index % SIZE;
+    let row: usize = index / SIZE + 1;
+    return format!("{}{}", ((column + 97) as u8) as char, row);
 }
